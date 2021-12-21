55db42ffcdf498c51c7dcdcf179ee82741e75da9
Cleonir Silva <82469705+cleosilva@users.noreply.github.com>
2021-12-21 13:51:38 -0300

    implementando modulo comunicação

---
+++ src/moduloComunicacaoDados.js
@@ -0,0 +1,32 @@
+const comunicacaoModal = [
+    {
+        idModal: 'camposVazios1', 
+        title: 'Campos vazios', 
+        bodyModal: 'Os campos não podem estar vazios!', 
+        b1: 'Fechar', 
+        b2: 'Salvar'
+    },
+    {
+        idModal: 'usuarioNaoExiste1', 
+        title: 'Não encontramos o usuário', 
+        bodyModal: 'Não é possível entrar com o usuário solicitado', 
+        b1: 'Fechar', 
+        b2: 'Criar'
+    },
+    {
+        idModal: 'loginExecutado1', 
+        title: 'Parabéns você está logado', 
+        bodyModal: 'As informações foram confirmadas!', 
+        b1: 'Fechar', 
+        b2: 'GoodPay Home'
+    },
+    {
+        idModal: 'falhaLogin1', 
+        title: 'Não foi possíevl logar', 
+        bodyModal: 'Erro nas informações de login', 
+        b1: 'Fechar', 
+        b2: 'Esqueci a senha'
+    },
+]
+
+export default comunicacaoModal;
