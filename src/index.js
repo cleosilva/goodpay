66ffeb5176e93c3665e71944d9c30a067555bc02
Cleonir Silva <82469705+cleosilva@users.noreply.github.com>
2021-12-22 14:01:10 -0300

    implementação do controller de rotas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import controlerUserLogin from "./moduloInterface.js";
+import controlerUserLogin from "./moduloControleLogin.js";
+import controleRotasApp from "./moduloControleRotas.js";
 
 // Trazendo elementos do Bootstrap 
+// Trazendo os botões da DOM em Html
 var myModal = new bootstrap.Modal(document.getElementById('myModal'))
 
-// Trazendo os botões da DOM em Html
+
+// Ordem para 
 const entrarUsuario = document.getElementById('btnEntrar');
 entrarUsuario.addEventListener('click', (event) => {
     let user = document.getElementById('usuario').value,
@@ -17,5 +20,20 @@ entrarUsuario.addEventListener('click', (event) => {
     document.getElementById('btnModalSave').innerHTML = alertModal.b2;
 
     myModal.show();
-})
+    setTimeout(carregaPagina, 2500)
+    function carregaPagina(){
+        window.location.href=controleRotasApp.validaRota(localStorage.status, alertModal.idModal);
+    }
+});
+
+// Ordem para criar um novo usuário
+const novoUsuario = document.getElementById('btnNovoUsuario');
+novoUsuario.addEventListener('click', (event) => {
+    window.location.href=controleRotasApp.validaRota('false', 'usuarioNaoExiste1');
+});
+
+
+
+
+
 
